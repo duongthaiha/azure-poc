b128193624a8134f637ee06f3b63b355caa38f1f
Ha Duong <haduong@microsoft.com>
2023-07-27 00:29:14 +0100

    Add AI telemetry

--- aks-inter-cluster-comm/FrontEndAPI/Program.cs
+++ aks-inter-cluster-comm/FrontEndAPI/Program.cs
@@ -10,6 +10,8 @@ namespace FrontEndAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddApplicationInsightsTelemetry();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
